3f07fe164b61250c867ede86ca389f48a3ec291e
Blockedlime9844 <itsyashvardhan76@gmail.com>
2024-11-01 22:13:53 +0530

    init

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "me.justlime"
+version = "1.0-EAP"
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
+        name = "spigotmc-repo"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+// Task to copy the jar to the server plugins folder
+tasks.register<Copy>("copyToServerPlugins") {
+    dependsOn("shadowJar")  // Ensure shadowJar completes before copying
+    from("$buildDir/libs/RedeemX-${project.version}-all.jar")  // Source jar file
+    into("E:/Minecraft/servers/Plugin-Maker/plugins")      // Destination folder
+}
+
+// Combined task to build and copy
+tasks.register("buildAndCopy") {
+    dependsOn("build", "copyToServerPlugins")
+}
---
+++ src/main/kotlin/me/justlime/redeemX/RedeemX.kt
@@ -0,0 +1,14 @@
+package me.justlime.redeemX
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RedeemX : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
